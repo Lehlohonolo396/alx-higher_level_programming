132fbcf31b6f5baddeadd968463e89dc2df06e11
Lehlohonolo396 <tlhogi928@gmail.com>
2024-04-17 02:49:19 -0700

    0x0B

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Defines a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Return the dictionary represnting a simple data structure."""
+    return obj.__dict__
+
